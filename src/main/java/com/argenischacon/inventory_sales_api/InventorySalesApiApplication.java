a9e2ac945a17cd43a88e08248ccceafb39626087
argenischaconb <argenischaconb@gmail.com>
2025-09-11 20:56:44 -0400

    refactor(config): make JPA auditing conditional by moving to dedicated config class
    - Move @EnableJpaAuditing annotation from main application class to new JpaAuditingConfig class
    - Add @ConditionalOnProperty to enable/disable auditing via application properties
    - Improve modularity and separation of concerns
    - Maintain backward compatibility with default enabled state

--- src/main/java/com/argenischacon/inventory_sales_api/InventorySalesApiApplication.java
+++ src/main/java/com/argenischacon/inventory_sales_api/InventorySalesApiApplication.java
@@ -2,10 +2,8 @@ package com.argenischacon.inventory_sales_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class InventorySalesApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/argenischacon/inventory_sales_api/config/JpaAuditingConfig.java
@@ -0,0 +1,11 @@
+package com.argenischacon.inventory_sales_api.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+@ConditionalOnProperty (prefix = "app.jpa", name = "auditing-enabled", havingValue = "true", matchIfMissing = true)
+public class JpaAuditingConfig {
+}
