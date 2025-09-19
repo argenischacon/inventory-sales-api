86b4f8ba42e674fc35c1726d3594f02d5d90238a
argenischaconb <argenischaconb@gmail.com>
2025-09-19 12:01:23 -0400

    feat(security): implement UserDetailsService for Spring Security authentication

---
+++ src/main/java/com/argenischacon/inventory_sales_api/security/UserDetailServiceImpl.java
@@ -0,0 +1,33 @@
+package com.argenischacon.inventory_sales_api.security;
+
+import com.argenischacon.inventory_sales_api.model.User;
+import com.argenischacon.inventory_sales_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.toString()))
+                        .collect(Collectors.toSet())
+        );
+    }
+}
