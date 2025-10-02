e7b552a3c2fdbedb44c5529d78a34ea737111b82
argenischaconb <argenischaconb@gmail.com>
2025-10-02 15:14:54 -0400

    docs(exception): add OpenAPI schema to ErrorResponse

--- src/main/java/com/argenischacon/inventory_sales_api/exception/ErrorResponse.java
+++ src/main/java/com/argenischacon/inventory_sales_api/exception/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.argenischacon.inventory_sales_api.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,10 +15,22 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "Standard structure for API error responses")
 public class ErrorResponse {
+
+    @Schema(description = "Timestamp when the error occurred", example = "2024-08-01T10:30:00.123456")
     private LocalDateTime timestamp;
+
+    @Schema(description = "HTTP status code", example = "404")
     private int status;
+
+    @Schema(description = "HTTP status error reason phrase", example = "Not Found")
     private String error;
+
+    @Schema(description = "A human-readable message providing details about the error", example = "Product not found")
     private String message;
+
+    @Schema(description = "A map containing additional details about the error, such as validation failures",
+            example = "{\"fieldName\": \"Error description\"}")
     private Map<String, Object> details;
 }
